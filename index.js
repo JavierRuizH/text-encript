6c3a59398e9a9d51881de2890cc5f2fd3218f93b
Javier Ruiz Hoyos <jav.ruiz200@gmail.com>
2023-04-23 01:40:51 -0500

    Adición de la funcionalidad para desencriptar el texto, ademas de mejorar el css de los botones

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ function encriptar() {
     } else {
         imagenEncriptado.src = "./img/cerrojo.png";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
-        alert("Debes ingresar algún texto");
+        swal("Ooops!", "Debes ingresar un texto", "warning");
     }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagenEncriptado = document.getElementById("imagenEncriptado");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        imagenEncriptado.src = "./img/desencriptado.jpg";
+      } else {
+        imagenEncriptado.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
