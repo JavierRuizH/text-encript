b74ee37595e8e21139b871201e7578b79be9e5a3
Javier Ruiz Hoyos <jav.ruiz200@gmail.com>
2023-05-14 17:21:12 -0500

    comentarios Readme y JS

---
+++ README.md
@@ -0,0 +1,35 @@
+# Encriptador de Texto
+
+Este proyecto es un encriptador de texto básico que te permite encriptar y desencriptar mensajes utilizando un conjunto específico de reemplazos de letras.
+
+## Características
+
+- Encripta mensajes sustituyendo ciertas letras por cadenas específicas.
+- Desencripta mensajes restaurando las letras originales.
+- Interfaz sencilla y fácil de usar.
+- Admite solo letras minúsculas sin acentos.
+- Muestra mensajes de éxito o advertencia según el resultado de la encriptación o desencriptación.
+
+## Tecnologías utilizadas
+
+- HTML
+- CSS
+- JavaScript
+- SweetAlert (librería para mostrar mensajes modales)
+
+## Cómo usar
+
+1. Ingresa el texto que deseas encriptar en el área de texto.
+2. Haz clic en el botón "Encriptar" para encriptar el texto o en el botón "Desencriptar" para desencriptarlo.
+3. El resultado de la encriptación o desencriptación se mostrará en el área de texto y se actualizarán los mensajes correspondientes.
+
+## Contribución
+
+Si deseas contribuir a este proyecto, puedes seguir los siguientes pasos:
+
+1. Haz un fork del repositorio.
+2. Crea una rama con tu nueva característica (`git checkout -b nueva-caracteristica`).
+3. Realiza los cambios necesarios y realiza un commit de tus cambios (`git commit -am 'Agrega nueva característica'`).
+4. Envía tus cambios a tu repositorio fork (`git push origin nueva-caracteristica`).
+5. Abre una Pull Request en este repositorio.
+
--- index.js
+++ index.js
@@ -1,47 +1,72 @@
 function encriptar() {
-    let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let imagenEncriptado = document.getElementById("imagenEncriptado");
+  // Obtiene el valor del texto ingresado por el usuario
+  let texto = document.getElementById("texto").value;
 
-    let textoCifrado = texto
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat");
+  // Obtiene referencias a elementos HTML
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let imagenEncriptado = document.getElementById("imagenEncriptado");
 
-    if (texto.length != 0) {
-        document.getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto encripdato con éxito";
-        parrafo.textContent = "";
-        imagenEncriptado.src = "./img/encriptado.jpg";
-    } else {
-        imagenEncriptado.src = "./img/cerrojo.png";
-        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
-        swal("Ooops!", "Debes ingresar un texto", "warning");
-    }
+  // Encripta el texto reemplazando vocales seguidas de "g" o "i" con cadenas específicas
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  // Verifica si el texto ingresado no está vacío
+  if (texto.length != 0) {
+    // Actualiza el valor del elemento de entrada de texto con el texto encriptado
+    document.getElementById("texto").value = textoCifrado;
+
+    // Establece el texto del elemento "titulo-mensaje" como "Texto encriptado con éxito"
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+
+    // Borra el contenido del elemento "parrafo"
+    parrafo.textContent = "";
+
+    // Establece la ruta de la imagen en "imagenEncriptado" como "./img/encriptado.jpg"
+    imagenEncriptado.src = "./img/encriptado.jpg";
+  } else {
+    // Si el texto está vacío, muestra un mensaje de error
+    imagenEncriptado.src = "./img/cerrojo.png";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
 }
 
 function desencriptar() {
-    let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
-    let imagenEncriptado = document.getElementById("imagenEncriptado");
-  
-    let textoCifrado = texto
-      .replace(/enter/gi, "e")
-      .replace(/imes/gi, "i")
-      .replace(/ai/gi, "a")
-      .replace(/ober/gi, "o")
-      .replace(/ufat/gi, "u");
-    
-      if (texto.length != 0) {
-        document.getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto desencriptado con éxito";
-        parrafo.textContent = "";
-        imagenEncriptado.src = "./img/desencriptado.jpg";
-      } else {
+  // Obtiene el valor del texto ingresado por el usuario
+  let texto = document.getElementById("texto").value;
+
+  // Obtiene referencias a elementos HTML
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let imagenEncriptado = document.getElementById("imagenEncriptado");
+
+  // Desencripta el texto reemplazando las cadenas encriptadas por las vocales correspondientes
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+  // Verifica si el texto ingresado no está vacío
+  if (texto.length != 0) {
+    // Actualiza el valor del elemento de entrada de texto con el texto desencriptado
+    document.getElementById("texto").value = textoCifrado;
+
+    // Establece el texto del elemento "titulo-mensaje" como "Texto desencriptado con éxito"
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+
+    // Borra el contenido del elemento "parrafo"
+    parrafo.textContent = "";
+
+    // Establece la ruta de la imagen en "imagenEncriptado" como "./img/desencriptado.jpg"
+    imagenEncriptado.src = "./img/desencriptado.jpg";
+  } else {
         imagenEncriptado.src = "./img/cerrojo.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
